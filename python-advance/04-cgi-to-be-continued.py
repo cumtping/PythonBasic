3e3603f86d950e130b0386d6e1bd73bb46d6df3c
WenpingWang <wenping0820@163.com>
2017-09-04 22:41:18 +0800

    cgi, to be continued.

---
+++ python-advance/04-cgi-to-be-continued.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+print ("Content-type:text/html")
+print ('')
+print ('<html>')
+print ('<head>')
+print ('<meta charset="utf-8">')
+print ('<title>Hello Word - My first cgi</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello Word! I am from cainiao</h2>')
+print ('</body>')
+print ('</html>')
